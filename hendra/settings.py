e109cdd92139c252633a2084e56eb7f6904c8f2a
Hendra Dwi Saputra <babeguru@gmail.com>
2024-11-24 22:51:15 +0700

    Add media directory and url

--- hendra/settings.py
+++ hendra/settings.py
@@ -89,3 +89,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'https://cdn.hendra.cloud/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
